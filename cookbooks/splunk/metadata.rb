c1955c0df0ed74540b023d954d82f619d4d15d0c
fmax <markus.froehlich@xidras.com>
2013-04-09 16:21:46 +0000

    added an input to the splunk cookbook

--- cookbooks/splunk/metadata.rb
+++ cookbooks/splunk/metadata.rb
@@ -6,3 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends "rightscale"
 recipe "splunk::install", "Installs splunk from a tar a file. Uses attributes and definitions"
+
+attribute "splunk/user",
+ :display_name => "Splunk User",
+  :description => "The name of the Splunk user",
+   :recipes => ["splunk::install"],
+    :required => "required"
